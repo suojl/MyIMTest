a79b7e90ddce21b7ae3c549c15932d4f40da7b0b
suojinglong <suojinglong@idengyun.com>
2017-11-09 18:06:31 +0800

    Initial Commit

---
+++ MyIMTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyIMTest
+//
+//  Created by suojl on 2017/11/9.
+//  Copyright © 2017年 com.dengyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyIMTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyIMTest
+//
+//  Created by suojl on 2017/11/9.
+//  Copyright © 2017年 com.dengyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
